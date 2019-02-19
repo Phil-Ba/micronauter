461a9688231182d71ad0458d9fe8d2201636221d
philba <p.bayer@freemail.hu>
2019-02-19 22:07:30 +0100

    new kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "at.bayava"
 version = "1.0-SNAPSHOT"
 val kotlinVersion = "1.3.10"
-val micronautVersion = "1.0.3"
+val micronautVersion = "1.0.4"
 val micronautTestVersion = "1.0.2"
 
 plugins {
@@ -49,23 +49,23 @@ compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
     javaParameters = true
 }
-//configure<JavaPluginConvention> {
-//    sourceCompatibility = JavaVersion.VERSION_1_8
-//}
 
 dependencies {
+
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-//    compile(kotlin("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"))
-//    compile(kotlin("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"))
-    implementation("io.micronaut:micronaut-inject:$micronautVersion")
 
-    annotationProcessor("io.micronaut:micronaut-inject-java:$micronautVersion")
-    kapt("io.micronaut:micronaut-inject-java:$micronautVersion")
+    implementation(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    implementation("io.micronaut:micronaut-inject")
+    implementation("io.micronaut:micronaut-core")
+    implementation("io.micronaut:micronaut-runtime")
 
-    kaptTest("io.micronaut:micronaut-inject-java:$micronautVersion")
+    annotationProcessor("io.micronaut:micronaut-inject-java")
+    kapt("io.micronaut:micronaut-inject-java")
+
+    kaptTest("io.micronaut:micronaut-inject-java")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.0")
-    testImplementation("io.micronaut:micronaut-inject:$micronautVersion")
+//    testImplementation("io.micronaut:micronaut-inject")
     testImplementation("org.mockito:mockito-junit-jupiter:2.22.0")
     testImplementation("io.micronaut.test:micronaut-test-junit5:$micronautTestVersion")
 }
